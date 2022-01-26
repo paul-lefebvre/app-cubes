8a249f8e9ca282d5293ae33199ee06e58950040d
Paul LEFEBVRE <lefebvre@antadis.com>
2022-01-26 11:42:49 +0100

    add android ios folders

---
+++ ios/borneapp/AppDelegate.h
@@ -0,0 +1,20 @@
+#import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
+
+//#import <Foundation/Foundation.h>
+//#import <EXUpdates/EXUpdatesAppController.h>
+//#import <React/RCTBridgeDelegate.h>
+//#import <UIKit/UIKit.h>
+//
+//#import <UMCore/UMAppDelegateWrapper.h>
+//
+//@interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate, EXUpdatesAppControllerDelegate>
+//
+//@end
---
+++ ios/empty.swift
@@ -0,0 +1,8 @@
+//
+//  empty.swift
+//  borneapp
+//
+//  Created by Antadis on 06/05/2021.
+//
+
+import Foundation
